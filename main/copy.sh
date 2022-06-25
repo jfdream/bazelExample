ce967d1a183ce14781f94788b73cfc5ba1bce77e
badwin <yangyudong@linkv.sg>
2022-06-25 10:14:46 +0800

    第三方库支持

--- main/copy.sh
+++ main/copy.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-bazel build -c dbg --config=darwin_arm64 main:libmediapipe.dylib
+bazel build -c dbg --config=darwin_arm64 main:libmediapipe.dylib --sandbox_debug
 rm -f /Users/yangyudong/Desktop/mediapipe/mediapipe/libmediapipe.dylib
 cp bazel-bin/main/libmediapipe.dylib_lipobin /Users/yangyudong/Desktop/mediapipe/mediapipe/libmediapipe.dylib
 cd /Users/yangyudong/Desktop/mediapipe/mediapipe
